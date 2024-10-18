9ef5e35c77da631a07525ae2352f54244fef7645
Reza-Golnari <i.am.reza.programmer@gmail.com>
2024-10-18 17:03:38 +0330

    header animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
-import {gsap} from 'gsap';
+import { gsap } from "gsap";
 
+gsap.fromTo('#navbar' , {
+    opacity: 0,
+    yPercent: -100,
+} , {
+    opacity: 1,
+    yPercent: 0,
+    duration: 1,
+})
+
+gsap.fromTo('#headerContent *' , {
+    opacity: 0,
+    xPercent: -200,
+} , {
+    opacity: 1,
+    xPercent: 0,
+    duration: 1,
+    stagger: .2,
+})
